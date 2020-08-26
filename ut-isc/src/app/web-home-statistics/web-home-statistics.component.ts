c79c40ca38815481125c35272c78c159b0b8def1
Ivan Blasco <ivan.blasco@maxiproducts.es>
2020-08-26 11:43:20 +0200

    Working statitistics - need images

--- ut-isc/src/app/web-home-statistics/web-home-statistics.component.ts
+++ ut-isc/src/app/web-home-statistics/web-home-statistics.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-web-home-statistics',
@@ -11,5 +12,39 @@ export class WebHomeStatisticsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  increaseCounter(): void {
+    console.log('Running counter');
+    $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 5000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+    });
+  }
+
+  @ViewChild('statistics') m: ElementRef;
+  private isVisible: boolean = false;
+
+  ngAfterViewInit(): void
+  {
+      if (this.isVisible == false && this.m.nativeElement.offsetParent != null)
+      {
+          console.log('isVisible switched from false to true');
+          this.isVisible = true;
+          this.increaseCounter();
+      }
+      else if (this.isVisible == true && this.m.nativeElement.offsetParent == null)
+      {
+          console.log('isVisible switched from true to false');
+          this.isVisible = false;
+      }
+  }
+
 
 }
